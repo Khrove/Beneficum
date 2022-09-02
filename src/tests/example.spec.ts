60084b2ad3a21b2d62dd40161434421da6f94b5c
khrove <joshualjohnson33@gmail.com>
2022-09-01 21:27:56 -0400

    Reverted

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -1,18 +1,18 @@
 import 'dotenv/config';
-import { test, expect } from '@playwright/test';
-import { AuthOperation } from '../components/business-operations/auth/Auth.operation';
+import { expect, test } from '@playwright/test';
 import { GradeViewOperation } from '../components/business-operations/explore/Grade.view.operations';
-import { delay } from '../../utils/CustomFunctions';
+import { AuthOperation } from '../components/business-operations/auth/Auth.operation';
 
-test('Login to Great Minds', async ({ page }) => {
-  const teacher = AuthOperation(page);
-  const gradeViewOperation = GradeViewOperation(page);
+test('Login to Great Minds and move around', async ({ page }) => {
+    const teacher = AuthOperation(page);
+    const gradeViewOps = GradeViewOperation(page);
 
-  await page.goto(`${process.env.UAT_URL}`);
-  await page.waitForLoadState('networkidle');
-  await teacher.loginWithEmail('sit_t1_reg1auto@yopmail.com', 'Test@123');
-  await page.waitForLoadState('networkidle');
-  expect(gradeViewOperation.areModulesVisible()).toBeTruthy();
-  await gradeViewOperation.checkGrade('Level 1');
-  await gradeViewOperation.clickOnModule('Counting, Comparison, and Addition');
-});
+    await page.goto(`${process.env.UAT_URL}`);
+    await page.waitForLoadState('networkidle');
+    await teacher.loginWithEmail('sit_t1_reg1auto@yopmail.com', 'Test@123');
+    await page.waitForLoadState('networkidle');
+    
+    expect(gradeViewOps.areModulesVisible()).toBeTruthy();
+    await gradeViewOps.checkGrade('Level 1');
+    await gradeViewOps.clickOnModule('Counting, Comparison, and Addition');
+  });
